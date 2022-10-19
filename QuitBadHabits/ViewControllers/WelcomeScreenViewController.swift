210bb3bb716d34ba00b2402eee8baa2d32f26ec4
kwartal <bv.barinov@gmail.com>
2022-10-19 14:36:05 +0300

    currect

--- QuitBadHabits/ViewControllers/WelcomeScreenViewController.swift
+++ QuitBadHabits/ViewControllers/WelcomeScreenViewController.swift
@@ -28,10 +28,17 @@ final class WelcomeScreenViewController: UIPageViewController, AVAudioPlayerDele
         addSubviews()
         setupSubviews()
         configureConstraints()
-        player?.delegate = self
-        player?.numberOfLoops = 0
         
+        let firstPage = ViewController1()
+        let secondPage = ViewController2()
+        let thirdPage = ViewController3()
+        
+        pages.append(firstPage)
+        pages.append(secondPage)
+        pages.append(thirdPage)
+
     }
+    
     
 }
 
@@ -136,55 +143,5 @@ extension WelcomeScreenViewController {
     
 }
 
-    //MARK: - UIPageViewControllerDataSource
 
-extension WelcomeScreenViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
-        
-        if currentIndex == 0 {
-            return pages.last
-        } else {
-            return pages[currentIndex - 1]
-        }
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        
-        guard let currentIndex = pages.firstIndex(of: viewController) else { return nil }
-        
-        if currentIndex < pages.count - 1 {
-            return pages[currentIndex + 1]
-        } else {
-            return pages.first
-        }
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        
-        guard let viewControllers = pageViewController.viewControllers else { return }
-        guard let currentIndex = pages.firstIndex(of: viewControllers[0]) else { return }
-        
-            pageControl.currentPage = currentIndex
-    }
-    
-    
-    
-    private func setup() {
-                pageControl.addTarget(self, action: #selector(pageControlTapped), for: .valueChanged)
-        pageControl.currentPageIndicatorTintColor = .black
-        pageControl.numberOfPages = pages.count
-        pageControl.currentPage = initialPage
-        
-        let page1 = ViewController1()
-        let page2 = ViewController2()
-        let page3 = ViewController3()
-        
-        pages.append(page1)
-        pages.append(page2)
-        pages.append(page3)
-        
-        setViewControllers([pages[initialPage]], direction: .forward, animated: true, completion: nil)
 
-    }
-}
